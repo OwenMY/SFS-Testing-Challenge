c7024f6fc3b3989ed2349c5e0465a17d43178a35
Owen Yoshishige <owenyoshishige@gmail.com>
2022-06-27 15:27:41 -0700

    create express server

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import React from 'react';
+
 const App = () => {
-  //ToDo
+  return (
+    <div>Hello All!!</div>
+  )
 };
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
-import { createRoot } from 'react-dom';
-import App from './App';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
